f27341ea19a6eea66a2783977be4c822c91ec736
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-10 16:53:14 -0400

    Create symptoms table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_204740) do
+ActiveRecord::Schema.define(version: 2020_09_10_205153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "symptoms", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "uid"
