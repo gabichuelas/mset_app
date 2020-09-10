88c7621ff26e0669b47f6c5b5a6522ba096ba200
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-10 17:17:14 -0400

    Add UserMedication

--- app/models/medication_symptom.rb
+++ app/models/medication_symptom.rb
@@ -1,5 +1,5 @@
 class MedicationSymptom < ApplicationRecord
-  validates :medication_id, :symptom_id
+  validates :medication_id, :symptom_id, presence: true
 
   belongs_to :medication
   belongs_to :symptom
