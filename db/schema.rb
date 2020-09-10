ad1e6f61b27a4776611a9a2c1a1a063c254d5635
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-10 16:59:23 -0400

    Create user_medications table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_205612) do
+ActiveRecord::Schema.define(version: 2020_09_10_205857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,15 @@ ActiveRecord::Schema.define(version: 2020_09_10_205612) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_medications", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "medication_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["medication_id"], name: "index_user_medications_on_medication_id"
+    t.index ["user_id"], name: "index_user_medications_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "uid"
     t.string "email"
@@ -55,4 +64,6 @@ ActiveRecord::Schema.define(version: 2020_09_10_205612) do
 
   add_foreign_key "logs", "symptoms"
   add_foreign_key "logs", "users"
+  add_foreign_key "user_medications", "medications"
+  add_foreign_key "user_medications", "users"
 end
--- spec/models/user_medication_spec.rb
+++ spec/models/user_medication_spec.rb
@@ -1,4 +1,4 @@
 RSpec.describe UserMedication do
   it { should belong_to :user }
-  it { should belong_to :user }
+  it { should belong_to :medication }
 end
