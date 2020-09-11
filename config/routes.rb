ce53c7841019d85cc6403ff038098220e1843e74
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-11 17:09:21 -0400

    Add route to sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
+
+  get '/auth/google_oauth2', to: 'sessions#omniauth'
 end
