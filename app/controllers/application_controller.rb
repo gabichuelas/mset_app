2cd2a6d621af690b18d8cbd4bfea6d5670781573
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-11 14:51:48 -0600

    Write test for outline of dashboard page; create dummy ApplicationController#current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    # to be deleted when michale's branch is merged
+  end
 end
---
+++ spec/dashboard/dashboard_index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'As an authenticated user' do
+  before :each do
+    @user = create(:user)
+  end
+
+  it 'I am redirected to the dashboard page after logging in.' do
+    # test being redirected to dashboard after login - might be covered by Michael's specs already
+  end
+
+  it 'On my dashboard, I see:
+      - profile section
+      - medications list section
+      - log a symptom section
+      - recent logs section
+      - logout button' do
+
+    # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    visit dashboard_path
+    expect(page).to have_css('.profile')
+    expect(page).to have_css('.med-list')
+    expect(page).to have_css('.log-form')
+    expect(page).to have_css('.recent-logs')
+    expect(page).to have_button("Logout")
+  end
+
+end
