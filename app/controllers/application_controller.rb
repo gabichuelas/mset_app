beafd379ca4efe48c218fa123c9ec914d2dcf095
Gaby Mendez <gmendez90@gmail.com>
2020-09-11 15:55:37 -0400

    Added current_user code to applicationController for use later when SessionsController is created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  # CODE WE WILL NEED TO IMPLEMENT CURRENT_USER ONCE
+  # SESSIONS CONTROLLER EXISTS, ETC.
+
+  # helper_method :current_user
+  #
+  # def current_user
+  #   @current_user ||= User.find_by(user_id: session[:user_id]) if session[:user_id]
+  # end
+  #
+  # def require_login
+  #   render file: '/public/404' unless current_user
+  # end
 end
