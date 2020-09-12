ee93eda4602d5fe87a8e8dfbf21add43f6f1a6f6
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-12 12:21:09 -0600

    Displays individualized birthdate and weight info in profile section

--- spec/features/dashboard/dashboard_index_spec.rb
+++ spec/features/dashboard/dashboard_index_spec.rb
@@ -20,11 +20,17 @@ RSpec.describe 'As an authenticated user' do
       - logout button' do
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-    visit "/dashboard"
+    visit dashboard_path
     expect(page).to have_css('.profile')
     expect(page).to have_css('.med-list')
     expect(page).to have_css('.log-form')
     expect(page).to have_css('.recent-logs')
     # expect(page).to have_button("Logout")
+  end
+
+  it 'In the profile section, I see my birthdate and my weight' do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    visit dashboard_path
+    expect(page).to have_content("1985-09-01")
   end
 end
