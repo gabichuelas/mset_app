deff900e3a4449a7f9ebb0fbf7bed7d6a111de46
Michael Evans <michaeljevans113@gmail.com>
2020-09-11 20:16:25 -0600

    Remove validation for refresh_token; attribute not present in Google auth hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
-  validates :uid, :email, :access_token, :refresh_token, presence: true
+  validates :uid, :email, :access_token, presence: true
 
   has_many :logs
   has_many :symptoms, through: :logs
+
+  def self.from_omniauth(user_info)
+    user_attributes = {
+      uid:           user_info[:uid],
+      email:         user_info[:info][:email],
+      first_name:    user_info[:info][:first_name],
+      last_name:     user_info[:info][:last_name],
+      access_token:  user_info[:credentials][:token],
+    }
+    user = User.create(user_attributes)
+    user
+  end
 end
