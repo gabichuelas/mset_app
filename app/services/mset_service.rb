305a3d61acfa4872554dbc3bc8f4dc063aa77f4f
Gaby Mendez <gmendez90@gmail.com>
2020-09-12 21:20:11 -0400

    Change connection in Mset_service to heroku domain

--- app/services/mset_service.rb
+++ app/services/mset_service.rb
@@ -15,10 +15,11 @@ class MsetService
   def conn
     # run $ rackup from mset_service_app
     # directory for this connection to work.
-    Faraday.new('http://localhost:9292')
+    # Faraday.new('http://localhost:9292')
 
     # using mset_service_app on heroku:
-    # will only work once mset_service is updated on heroku with latest PR. Faraday.new(ENV['MSET_API_SERVICE_DOMAIN'])
+    # will only work once mset_service is updated on heroku with latest PR.
+    Faraday.new(ENV['MSET_API_SERVICE_DOMAIN'])
   end
 
 end
