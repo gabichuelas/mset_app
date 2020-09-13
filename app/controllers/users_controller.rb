8f0b8db6e4f9c692d21451f58e748bd7ac425aa1
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-13 16:18:44 -0600

    Update action now checks for first/last name; add flash message partial to edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,15 @@ class UsersController < ApplicationController
   before_action :require_user
 
   def update
-    current_user.update(user_params)
-    if current_user.save
-      flash[:success] = 'Account details updated!'
-      redirect_to '/dashboard'
+    if missing_name?(user_params)
+      flash[:error] = 'You must save a first and last name'
+      redirect_to request.referer
+    else
+      current_user.update(user_params)
+      if current_user.save
+        flash[:success] = 'Account details updated!'
+        redirect_to '/dashboard'
+      end
     end
   end
 
@@ -15,5 +20,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.permit(:first_name, :last_name, :birthdate, :weight)
+  end
+
+  def missing_name?(user_params)
+    user_params[:first_name].empty? || user_params[:last_name].empty?
   end
 end
