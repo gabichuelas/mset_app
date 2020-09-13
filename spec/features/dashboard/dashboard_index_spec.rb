e9cef539806f6e221aa9e7c8994329865cfcf6cf
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-12 19:31:43 -0600

    Create scenario for no meds on list

--- spec/features/dashboard/dashboard_index_spec.rb
+++ spec/features/dashboard/dashboard_index_spec.rb
@@ -28,9 +28,23 @@ RSpec.describe 'As an authenticated user' do
     expect(page).to have_content('Sign Out')
   end
 
-  it 'In the profile section, I see my birthdate and my weight' do
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-    visit dashboard_path
-    expect(page).to have_content("1985-09-01")
+  describe 'On my dashboard' do
+    before :each do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      visit dashboard_path
+    end
+
+    it 'In the profile section, I see my birthdate and my weight' do
+      within('.profile') do
+        expect(page).to have_content("1985-09-01")
+      end
+    end
+
+    it 'If I don\'t have any meds on my list, the medications list section has a note that indicates this and instructs me to add to the list' do
+      within('.med-list') do
+        expect(page).to have_content("You don't have any saved medications.")
+        expect(page).to have_button("Add Medications")
+      end
+    end
   end
 end
