94445856c6945f03f55c362b61a430f447f12dda
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-13 23:34:21 -0600

    Define @potential_symptoms in Dashboard#index

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   before_action :require_user
 
-  def index; end
+  def index
+    @potential_symptoms = current_user.potential_symptoms
+  end
 end
