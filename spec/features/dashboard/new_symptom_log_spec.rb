c39b94c0e9da6354395f5520c11c62996dfac1dd
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-14 00:22:48 -0600

    Outline sad path test and other test cases for logging new symptom

--- spec/features/dashboard/new_symptom_log_spec.rb
+++ spec/features/dashboard/new_symptom_log_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'As an authenticated user, when I visit my dashboard,' do
-  it 'I can log a new symptom, and see a flash message confirming the new log as well as the log displayed in the recent logs section' do
+  before :each do
     user = create(:user)
     medication = create(:medication)
     symptom_1 = Symptom.create!(description: "Headache")
@@ -13,6 +13,29 @@ RSpec.describe 'As an authenticated user, when I visit my dashboard,' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit dashboard_path
+  end
+
+  it 'I can log a new symptom, and see a flash message confirming the new log as well as the log displayed in the recent logs section' do
+    within('.log-form') do
+      select "Headache", from: :symptom
+      fill_in :when, with: "2020-09-13T23:09"
+      fill_in :note, with: "7/10 pain scale"
+      click_button "Save"
+    end
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("New symptom logged!")
+
+    # within('.recent-logs') do
+    #   expect(page).to have_content("Headache")
+    #   expect(page).to have_content("2020-09-13T23:09")
+    #   expect(page).to have_content(7/10 pain scale")
+    # end
+    # the spec seems to not register that current_user now has a new log relationship
+    # works in server but not in the test
+  end
+
+  it 'I can successfully log a new symptom without including a note' do
     within('.log-form') do
       select "Headache", from: :symptom
       fill_in :when, with: "2020-09-13T23:09"
@@ -26,5 +49,48 @@ RSpec.describe 'As an authenticated user, when I visit my dashboard,' do
     # within('.recent-logs') do
     #   expect(page).to have_content("Headache")
     # end
+    # same issue as above
+  end
+
+  it 'If I try to log a new symptom without selecting a symptom, I receive an error' do
+    within('.log-form') do
+      fill_in :when, with: "2020-09-13T23:09"
+      fill_in :note, with: "7/10 pain scale"
+      click_button "Save"
+    end
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content('Please specify a symptom')
+  end
+
+  it 'If I try to log a new symptom without selecting a date/time, I receive an error' do
+    within('.log-form') do
+      select "Headache", from: :symptom
+      fill_in :note, with: "7/10 pain scale"
+      click_button "Save"
+    end
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content('Please specify when you experienced this symptom')
+  end
+
+  xit 'I can submit a log for an unlisted symptom' do
+    within('.log-form') do
+      select "Other", from: :symptom
+      # mvp UX: if user selects "other" they are required to include a note
+      # ideal UX: there's a new form field that appears if "other" is selected and then the user is required to specify what the symptom was
+      fill_in :when, with: "2020-09-13T23:09"
+      fill_in :note, with: "Migraine - pain scale"
+      click_button "Save"
+    end
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("New symptom logged!")
+
+    # within('.recent-logs') do
+    #   expect(page).to have_content("Other")
+    #   expect(page).to have_content("Migraine")
+    # end
+    # same issue as above
   end
 end
