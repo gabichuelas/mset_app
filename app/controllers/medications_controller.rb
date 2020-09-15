2bc60bfec88ae65be5fd5716908010529204b9bc
Gaby Mendez <gmendez90@gmail.com>
2020-09-14 21:25:07 -0400

    Add conditional to check for duplicate medications on a user

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -9,11 +9,16 @@ class MedicationsController < ApplicationController
   end
 
   def create
-    medication = current_user.medications.create(med_params)
-    symptoms = SEARCH_RESULTS.extract_symptoms(medication.product_ndc)
-    unless symptoms.nil?
-      medication.save_symptoms(symptoms)
-      flash[:success] = "#{medication.brand_name} has been added to your medication list!"
+    medication = Medication.find_or_create_by(med_params)
+    if !current_user.has_medication?(medication.id)
+      UserMedication.create(user_id: current_user.id, medication_id: medication.id)
+      symptoms = SEARCH_RESULTS.extract_symptoms(medication.product_ndc)
+      unless symptoms.nil?
+        medication.save_symptoms(symptoms)
+        flash[:success] = "#{medication.brand_name} has been added to your medication list!"
+      end
+    else
+      flash[:warning] = "#{medication.brand_name} has already been added to your medication list."
     end
     redirect_to '/dashboard'
   end
