5ada54071efb5b9f971bb3ddbfadf8abc6e8dc59
Gaby Mendez <gmendez90@gmail.com>
2020-09-14 21:18:24 -0400

    Write spec for user#has_medication?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,9 +8,21 @@ RSpec.describe User do
   it { should have_many :user_medications }
   it { should have_many(:medications).through(:user_medications) }
 
+  describe "instance methods" do
+    before :each do
+      @user = create(:user, first_name: "Joe", last_name: "Doe")
+    end
 
-  it "user#full_name" do
-    user = create(:user, first_name: "Joe", last_name: "Doe")
-    expect(user.full_name).to eq("Joe Doe")
+    it "user#full_name" do
+      expect(@user.full_name).to eq("Joe Doe")
+    end
+
+    it "user#has_medication?" do
+      medication = create(:medication)
+      medication2 = create(:medication)
+      UserMedication.create(user_id: @user.id, medication_id: medication.id)
+      expect(@user.has_medication?(medication.id)).to eq(true)
+      expect(@user.has_medication?(medication2.id)).to eq(false)
+    end
   end
 end
