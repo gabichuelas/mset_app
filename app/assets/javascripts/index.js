bf8f3ad8204f1d3bb93eba2960428a84a001029e
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-15 16:52:09 -0400

    Remove more jQuery code

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,9 +3,3 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
-$(document).ready(function(){
-  $('[data-js-search]').change(function(event) {
-    search_term = $(this).val();
-    alert('You are searching for ' + search_term);
-  });
-});
