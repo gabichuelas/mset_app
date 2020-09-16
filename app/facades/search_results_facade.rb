512320443f614cfd511c9f6519075ed6eb2ddf42
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-16 11:52:36 -0600

    Remove unused parsing_conditions method

--- app/facades/search_results_facade.rb
+++ app/facades/search_results_facade.rb
@@ -46,21 +46,6 @@ class SearchResultsFacade
     end
   end
 
-  # def parsing_conditions(element)
-  #   return true if element.text.downcase=~/\d|%|only|hctz|
-  #   road|traffic|system|general|digestive|
-  #   musculo-skeletal|urogenital|gastrointestinal|
-  #   cramps leg|other|fatal|musculoskeletal|cutaneous|
-  #   patients|surgical|none reported|special|
-  #   dermatological|psychiatric|neurologic|major|
-  #   major noncerebral|respiratory|disorders|
-  #   reactions|table|metabolic|nutritional|
-  #   adverse|event|placebo/ ||
-  #   element.name == 'footnote' ||
-  #   element.text == ' ' ||
-  #   element.text.split(' ').size > 3
-  # end
-
   def nokogiri_parser(table, acc)
     page = Nokogiri::XML(table)
     page.css('tbody').select do |node|
