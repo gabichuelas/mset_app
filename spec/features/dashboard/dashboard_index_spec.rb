c7028a495927fe32bf0a0a23e8ac0c34b5a0ec57
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-09-16 12:23:01 -0600

    Clean up

--- spec/features/dashboard/dashboard_index_spec.rb
+++ spec/features/dashboard/dashboard_index_spec.rb
@@ -80,29 +80,25 @@ RSpec.describe 'As an authenticated user' do
   end
 
   it 'In the recent-logs section, I see my recently logged symptoms' do
-    user = create(:user)
-
     symptom_1 = Symptom.create!(description: "Headache")
     symptom_2 = Symptom.create!(description: "Insomnia")
     symptom_3 = Symptom.create!(description: "Death")
 
     log_1_time = DateTime.new(2004,2,3,4,5,0, '+6:00') # second, headache
-    log_1 = Log.create(user: user, symptom: symptom_1, when: log_1_time)
+    log_1 = Log.create(user: @user, symptom: symptom_1, when: log_1_time)
 
     log_2_time = DateTime.new(2003,2,3,4,5,0, '+6:00') # third, insomnia
-    log_2 = Log.create(user: user, symptom: symptom_2, when: log_2_time)
+    log_2 = Log.create(user: @user, symptom: symptom_2, when: log_2_time)
 
     log_3_time = DateTime.new(2005,2,3,4,5,0, '+6:00') # first, death
-    log_3 = Log.create(user: user, symptom: symptom_3, when: log_3_time, note: 'make it stop!!!!')
+    log_3 = Log.create(user: @user, symptom: symptom_3, when: log_3_time, note: 'make it stop!!!!')
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
     visit dashboard_path
-    # save_and_open_page
 
     within('.recent-logs') do
       within(first('.log')) do
-        # binding.pry
         expect(page).to have_css('.symptom')
         symptom = find('.symptom').text
         expect(symptom).to eq(symptom_3.description)
