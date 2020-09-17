f16d8ebe07ae38ceaa657811e72f2499f04679fa
Michael Evans <michaeljevans113@gmail.com>
2020-09-17 12:14:05 -0600

    Passing on production I guess

--- spec/features/dashboard/medications_spec.rb
+++ spec/features/dashboard/medications_spec.rb
@@ -60,11 +60,11 @@ RSpec.describe 'User can add a medication by name', type: :feature do
       click_on 'Find Medication'
 
       within('.medications', match: :first) do
-        click_on 'Hand Sanitizer'
+        click_on 'HAND SANITIZER'
       end
 
       expect(current_path).to eq('/dashboard')
-      expect(page).to have_content('Hand Sanitizer')
+      expect(page).to have_content('HAND SANITIZER')
 
       expect(Symptom.potential_symptoms(@user)).to be_empty
     end
