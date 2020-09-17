30d70b7d994536fa9f1b591bd3cc0ae98034a936
Michael Evans <michaeljevans113@gmail.com>
2020-09-17 11:03:10 -0600

    Add indexing to user uid

---
+++ db/migrate/20200917165847_add_index_to_user_uid.rb
@@ -0,0 +1,5 @@
+class AddIndexToUserUid < ActiveRecord::Migration[5.2]
+  def change
+    add_index :users, :uid, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_002726) do
+ActiveRecord::Schema.define(version: 2020_09_17_165847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_09_16_002726) do
     t.integer "weight"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
   add_foreign_key "logs", "symptoms"
